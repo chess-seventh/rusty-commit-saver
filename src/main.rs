edef6973077e7fa89c34d042b539a1c4248fd19b
Chess Seventh <chess7th@pm.me>
2023-02-11 00:20:15 +0100

    feat: first draft

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
+use git2::Repository;
+
 fn main() {
-    println!("Hello, world!");
+    let git_repo = Repository::discover("./").unwrap();
+    let head = git_repo.head().unwrap();
+    let commit = head.peel_to_commit().unwrap().id();
+    let branch_name = head.shorthand().unwrap();
+    println!("{commit:?}");
+    println!("{branch_name:?}");
 }
-
-// use std::env;
-// use std::io::{stdin, BufRead};
-
-// fn main() {
-//     log();
-// }
-
-// // This consumes stdin. Do not call this if you need to use stdin.
-// fn log() {
-//     let name_arg = env::args().nth(0).unwrap_or(String::from("unknown"));
-//     let args: Vec<_> = env::args().skip(1).collect();
-//     println!("{} called with {:?}", name_arg, args);
-//
-//     println!("BEGIN STDIN");
-//     let stdin = stdin();
-//     for line in stdin.lock().lines() {
-//         println!("{:?}", line);
-//     }
-//     println!("END STDIN");
-// }
