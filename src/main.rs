b88fdba9139384c43dcef2771339c8a5fb8e96c2
Chess Seventh <chess7th@pm.me>
2023-10-20 06:51:48 +0200

    feat: split into multiple files to make the code more readable

--- src/main.rs
+++ src/main.rs
@@ -2,122 +2,20 @@
 //! Save all you commits to your VimWiki
 //!
 
+pub mod vim_commit;
+use vim_commit::VimCommit;
+
 /// Standard Lib
 use std::env;
 use std::fs;
-use std::fs::OpenOptions;
-use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 
 /// External crates
 use chrono::DateTime;
-use chrono::NaiveDateTime;
 use chrono::Utc;
 use dirs::home_dir;
-use git2::Repository;
 
-#[derive(Debug, Clone)]
-struct VimCommit {
-    pub repository_url: String,
-    pub branch_name: String,
-    pub commit_hash: String,
-    pub commit_msg: String,
-    pub datetime: String,
-}
-
-/// Defaults for VimCommit
-impl Default for VimCommit {
-    fn default() -> VimCommit {
-        VimCommit {
-            repository_url: {
-                let git_repo = Repository::discover("./").unwrap();
-                let bind = git_repo.find_remote("origin").unwrap();
-                bind.url().unwrap().replace('\"', "")
-            },
-            branch_name: {
-                let git_repo = Repository::discover("./").unwrap();
-                let head = git_repo.head().unwrap();
-                head.shorthand().unwrap().replace('\"', "")
-            },
-            commit_hash: {
-                let git_repo = Repository::discover("./").unwrap();
-                let head = git_repo.head().unwrap();
-                let cbind = head.peel_to_commit().unwrap();
-                cbind.id().to_string()
-            },
-            commit_msg: {
-                let git_repo = Repository::discover("./").unwrap();
-                let head = git_repo.head().unwrap();
-                let cbind = head.peel_to_commit().unwrap();
-                let commit = cbind.message().unwrap().replace(['\n', '\"'], "");
-
-                match commit.char_indices().nth(120) {
-                    None => commit.to_string(),
-                    Some((idx, _)) => commit[..idx].to_string(),
-                }
-            },
-            datetime: {
-                let git_repo = Repository::discover("./").unwrap();
-                let head = git_repo.head().unwrap();
-
-                let cbind = head.peel_to_commit().unwrap();
-                let commit_date: i64 = cbind.time().seconds();
-                let dt: DateTime<Utc> = DateTime::from_utc(
-                    NaiveDateTime::from_timestamp_opt(commit_date, 0).unwrap(),
-                    Utc,
-                );
-                dt.format("%H:%M:%S").to_string()
-            },
-        }
-    }
-}
-
-impl VimCommit {
-    pub fn new() -> Self {
-        VimCommit::default()
-    }
-
-    /// Prepares input to write to vimwiki
-    fn prepare_input(&mut self) -> String {
-        format!(
-            "| {:} | {:} | {:} | {:} | {:} |\n",
-            self.datetime, self.commit_msg, self.repository_url, self.branch_name, self.commit_hash
-        )
-    }
-
-    /// Append git stuff to diary
-    fn append_commit_stuff_to_diary(&mut self, wiki: &PathBuf) {
-        let new_commit_str = self.prepare_input();
-
-        let mut file_ref = OpenOptions::new()
-            .append(true)
-            .open(wiki)
-            .expect("Unable to open wiki");
-
-        file_ref
-            .write_all(new_commit_str.as_bytes())
-            .expect("Failed to write the new commit string");
-
-        println!("Commit logged in: {:}", wiki.to_str().unwrap());
-    }
-
-    /// check if remote is for transics or else.
-    fn select_proper_diary(&mut self) -> String {
-        let diary_path = prepare_path_with_emojis();
-        if self.repository_url.contains("transics") {
-            return format!(".vimwiki/{diary_path:}/3. Field Work");
-        }
-        let diary_path = format!("{diary_path:}/0. Daily");
-        format!(".vimwiki/{diary_path:}/")
-    }
-}
-
-fn prepare_path_with_emojis() -> String {
-    let calendar = emojis::get("📅").unwrap();
-    let diary = format!("{calendar:} Diaries");
-    diary
-}
 /// Create file
 fn mkfile(wikidir: &PathBuf) {
     let today: DateTime<Utc> = Utc::now();
@@ -193,7 +91,7 @@ fn main() {
         return;
     }
 
-    let wikifile = vimc.select_proper_diary();
+    let wikifile = vimc.select_proper_diary_folder();
     let wiki = check_diary_day_exists(&wikifile);
     vimc.append_commit_stuff_to_diary(&wiki);
 }
---
+++ src/vim_commit.rs
@@ -0,0 +1,115 @@
+use chrono::DateTime;
+use chrono::NaiveDateTime;
+use chrono::Utc;
+use git2::Repository;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone)]
+pub struct VimCommit {
+    pub repository_url: String,
+    pub branch_name: String,
+    pub commit_hash: String,
+    pub commit_msg: String,
+    pub datetime: String,
+}
+
+/// Defaults for VimCommit
+impl Default for VimCommit {
+    fn default() -> VimCommit {
+        VimCommit {
+            repository_url: {
+                let git_repo = Repository::discover("./").unwrap();
+                let bind = git_repo.find_remote("origin").unwrap();
+                bind.url().unwrap().replace('\"', "")
+            },
+            branch_name: {
+                let git_repo = Repository::discover("./").unwrap();
+                let head = git_repo.head().unwrap();
+                head.shorthand().unwrap().replace('\"', "")
+            },
+            commit_hash: {
+                let git_repo = Repository::discover("./").unwrap();
+                let head = git_repo.head().unwrap();
+                let cbind = head.peel_to_commit().unwrap();
+                cbind.id().to_string()
+            },
+            commit_msg: {
+                let git_repo = Repository::discover("./").unwrap();
+                let head = git_repo.head().unwrap();
+                let cbind = head.peel_to_commit().unwrap();
+                let commit = cbind.message().unwrap().replace(['\n', '\"'], "");
+
+                match commit.char_indices().nth(120) {
+                    None => commit.to_string(),
+                    Some((idx, _)) => commit[..idx].to_string(),
+                }
+            },
+            datetime: {
+                let git_repo = Repository::discover("./").unwrap();
+                let head = git_repo.head().unwrap();
+
+                let cbind = head.peel_to_commit().unwrap();
+                let commit_date: i64 = cbind.time().seconds();
+                let dt: DateTime<Utc> = DateTime::from_utc(
+                    NaiveDateTime::from_timestamp_opt(commit_date, 0).unwrap(),
+                    Utc,
+                );
+                dt.format("%H:%M:%S").to_string()
+            },
+        }
+    }
+}
+
+impl VimCommit {
+    pub fn new() -> Self {
+        VimCommit::default()
+    }
+
+    /// Prepares input to write to vimwiki
+    fn prepare_input(&mut self) -> String {
+        format!(
+            "| {:} | {:} | {:} | {:} | {:} |\n",
+            self.datetime, self.commit_msg, self.repository_url, self.branch_name, self.commit_hash
+        )
+    }
+
+    /// Append git stuff to diary
+    pub fn append_commit_stuff_to_diary(&mut self, wiki: &PathBuf) {
+        let new_commit_str = self.prepare_input();
+
+        let mut file_ref = OpenOptions::new()
+            .append(true)
+            .open(wiki)
+            .expect("Unable to open wiki");
+
+        file_ref
+            .write_all(new_commit_str.as_bytes())
+            .expect("Failed to write the new commit string");
+
+        println!(
+            "Commit logged in ...................................................... {:}",
+            wiki.file_name()
+                .expect("No filename found while printing the log output")
+                .to_str()
+                .unwrap()
+        );
+    }
+
+    /// check if remote is for transics or else.
+    pub fn select_proper_diary_folder(&mut self) -> String {
+        let diary_path = prepare_path_with_emojis();
+        if self.repository_url.contains("transics") {
+            return format!(".vimwiki/{diary_path:}/3. Field Work");
+        }
+        let diary_path = format!("{diary_path:}/0. Daily");
+        format!(".vimwiki/{diary_path:}/")
+    }
+}
+
+fn prepare_path_with_emojis() -> String {
+    let calendar = emojis::get("📅").unwrap();
+    let diary = format!("{calendar:} Diaries");
+    diary
+}
