de4e214e19956ecbbed0375dda7707c56ef2605e
Chess Seventh <chess7th@pm.me>
2023-10-20 06:40:08 +0200

    feat: move methods from VimRC struct as they don't need internal variables

--- src/main.rs
+++ src/main.rs
@@ -86,70 +86,6 @@ impl VimCommit {
         )
     }
 
-    /// Method to veritfy that the file exists
-    /// will trigger the creation of it with a template if it doesn't
-    fn check_diary_day_exists(&mut self, vimwiki: &str) -> PathBuf {
-        let today: DateTime<Utc> = Utc::now();
-        let md_file = format!("{}.md", today.format("%Y-%m-%d"));
-        let mut wikidir = home_dir().unwrap();
-
-        // Recursively create a directory and all of its parent components if they are missing.
-        // https://stackoverflow.com/a/48053959
-        fs::create_dir_all(wikidir.clone().into_os_string().to_str().unwrap())
-            .expect("Couldn't create the directory");
-
-        wikidir.push(&[&vimwiki, &md_file.as_str()].iter().collect::<PathBuf>());
-
-        if !Path::new(&wikidir).exists() {
-            println!(
-                "Diary entry doesn't exist, we're now creating it: {:?}",
-                wikidir.as_os_str().to_str().unwrap()
-            );
-            self.mkfile(&wikidir);
-        }
-        wikidir
-    }
-
-    /// Create file
-    fn mkfile(&mut self, wikidir: &PathBuf) {
-        let today: DateTime<Utc> = Utc::now();
-        let md_title = if wikidir
-            .as_os_str()
-            .to_str()
-            .unwrap()
-            .to_string()
-            .contains("Work")
-        {
-            format!(
-                "# {}\n\n
-## AM\n\n
-## PM\n\n
-## Todays' commits:\n
-| TIME | COMMIT MESSAGE | REPOSITORY URL | BRANCH | COMMIT HASH |
-|------|----------------|----------------|--------|-------------|
-",
-                today.format("%Y-%m-%d")
-            )
-        } else {
-            format!(
-                "# {}\n\n
-## Food:\n\n
-- breakfast:\n
-- lunch:\n
-- dinner:\n\n
-## Personal notes:\n\n
-## Todays' commits:\n
-| TIME | COMMIT MESSAGE | REPOSITORY URL | BRANCH | COMMIT HASH |
-|------|----------------|----------------|--------|-------------|
-",
-                today.format("%Y-%m-%d")
-            )
-        };
-
-        fs::write(wikidir, md_title)
-            .expect("Something went wrong creating in writing things in the wikifile");
-    }
-
     /// Append git stuff to diary
     fn append_commit_stuff_to_diary(&mut self, wiki: &PathBuf) {
         let new_commit_str = self.prepare_input();
@@ -182,6 +118,69 @@ fn prepare_path_with_emojis() -> String {
     let diary = format!("{calendar:} Diaries");
     diary
 }
+/// Create file
+fn mkfile(wikidir: &PathBuf) {
+    let today: DateTime<Utc> = Utc::now();
+    let md_title = if wikidir
+        .as_os_str()
+        .to_str()
+        .unwrap()
+        .to_string()
+        .contains("Work")
+    {
+        format!(
+            "# {}\n\n
+## AM\n\n
+## PM\n\n
+## Todays' commits:\n
+| TIME | COMMIT MESSAGE | REPOSITORY URL | BRANCH | COMMIT HASH |
+|------|----------------|----------------|--------|-------------|
+",
+            today.format("%Y-%m-%d")
+        )
+    } else {
+        format!(
+            "# {}\n\n
+## Food:\n\n
+- breakfast:\n
+- lunch:\n
+- dinner:\n\n
+## Personal notes:\n\n
+## Todays' commits:\n
+| TIME | COMMIT MESSAGE | REPOSITORY URL | BRANCH | COMMIT HASH |
+|------|----------------|----------------|--------|-------------|
+",
+            today.format("%Y-%m-%d")
+        )
+    };
+
+    fs::write(wikidir, md_title)
+        .expect("Something went wrong creating in writing things in the wikifile");
+}
+
+/// Method to veritfy that the file exists
+/// will trigger the creation of it with a template if it doesn't
+fn check_diary_day_exists(vimwiki: &str) -> PathBuf {
+    let today: DateTime<Utc> = Utc::now();
+    let md_file = format!("{}.md", today.format("%Y-%m-%d"));
+    let mut wikidir = home_dir().unwrap();
+
+    // Recursively create a directory and all of its parent components if they are missing.
+    // https://stackoverflow.com/a/48053959
+    fs::create_dir_all(wikidir.clone().into_os_string().to_str().unwrap())
+        .expect("Couldn't create the directory");
+
+    wikidir.push(&[&vimwiki, &md_file.as_str()].iter().collect::<PathBuf>());
+
+    if !Path::new(&wikidir).exists() {
+        println!(
+            "Diary entry doesn't exist, we're now creating it: {:?}",
+            wikidir.as_os_str().to_str().unwrap()
+        );
+        mkfile(&wikidir);
+    }
+    wikidir
+}
 
 fn main() {
     let mut vimc = VimCommit::new();
@@ -195,6 +194,6 @@ fn main() {
     }
 
     let wikifile = vimc.select_proper_diary();
-    let wiki = vimc.check_diary_day_exists(&wikifile);
+    let wiki = check_diary_day_exists(&wikifile);
     vimc.append_commit_stuff_to_diary(&wiki);
 }
