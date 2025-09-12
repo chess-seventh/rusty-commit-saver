08091262d01d73870828707d54596cdc9ef7d636
Chess Seventh <25706993+chess-seventh@users.noreply.github.com>
2025-09-12 14:23:13 +0200

    feat!: Flakify this repository with some fixes
    * feat: Update Rust environment and dependencies
    
    - Modernize Rust ecosystem by updating to the 2024 edition and various
      dependency upgrades.
    - Streamline developer environment and tools in `.nix` files.
    - Improve diary entry management in `src/main.rs`.
    - Remove outdated configurations and enhance automated code quality
      checks.
    - Update project dependencies, including Rust edition and various
      library versions
    - Streamline development environment configuration and toolchain
      settings
    - Enhance code quality and project structure by refining Nix setup and
      removing obsolete code references in Rust files
    
    * refactor: Simplify log messages in main.rs
    
    - Streamline log outputs in `main.rs` for better readability and
      performance.

--- src/main.rs
+++ src/main.rs
@@ -73,8 +73,8 @@ fn create_diary_file(
 fn create_directories_for_new_entry(entry_directory_and_path: &Path) -> Result<(), Box<dyn Error>> {
     let parent_dirs = get_parent_from_full_path(entry_directory_and_path)?;
     fs::create_dir_all(parent_dirs)?;
-    info!("[INFO] Creating diary file & path ...........................");
-    println!("[INFO] Creating diary file & path ...........................");
+    info!("[INFO] Creating diary file & path ");
+    println!("[INFO] Creating diary file & path ");
 
     Ok(())
 }
@@ -84,11 +84,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let current_directory = env::current_dir().unwrap();
     let mut entry_directory_and_path = home_dir().unwrap();
-    entry_directory_and_path.push(".vimwiki");
+    entry_directory_and_path.push("Documents");
+    entry_directory_and_path.push("Wiki");
 
     if current_directory == entry_directory_and_path {
-        info!("[INFO] No need to save the commit here ......................");
-        println!("[INFO] No need to save the commit here ......................");
+        info!("[INFO] No need to save the commit here ");
+        println!("[INFO] No need to save the commit here ");
         return Ok(());
     }
 
@@ -98,25 +99,25 @@ fn main() -> Result<(), Box<dyn Error>> {
     let tmp = &entry_directory_and_path.as_os_str().to_str().unwrap();
 
     if let Ok(()) = check_diary_path_exists(&entry_directory_and_path) {
-        info!("[INFO] Diary file/path exists ...............................");
-        info!("[INFO] {tmp:} ...............................");
-        println!("[INFO] Diary file/path exists ...............................");
-        println!("[INFO] {tmp:} ...............................");
+        info!("[INFO] Diary file/path exists ");
+        info!("[INFO] {tmp:} ");
+        println!("[INFO] Diary file/path exists ");
+        println!("[INFO] {tmp:} ");
     } else {
-        info!("[INFO] Diary file/path DOES NOT exist .......................");
-        println!("[INFO] Diary file/path DOES NOT exist .......................");
+        info!("[INFO] Diary file/path DOES NOT exist ");
+        println!("[INFO] Diary file/path DOES NOT exist ");
         create_directories_for_new_entry(&entry_directory_and_path)?;
         create_diary_file(
             entry_directory_and_path.as_os_str().to_str().unwrap(),
             &mut commit_saver_struct,
         )?;
-    };
+    }
 
     // write commit
     match commit_saver_struct.append_entry_to_diary(&entry_directory_and_path) {
         Ok(()) => {
-            info!("[INFO] Commit logged in .....................................");
-            println!("[INFO] Commit logged in .....................................");
+            info!("[INFO] Commit logged in ");
+            println!("[INFO] Commit logged in ");
             Ok(())
         }
         Err(e) => {
