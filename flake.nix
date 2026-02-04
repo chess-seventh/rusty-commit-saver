ebaeb38987ea42790810375c2097e8b6e513494f
chess-seventh <chess7th@pm.me>
2026-02-04 12:22:03 +0000

    chore: update Cargo.lock, Cargo.toml, and flake.nix [skip ci]

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       in {
         packages.default = rustPlatform.buildRustPackage {
           pname = "rusty-commit-saver";
-          version = "4.8.0";
+          version = "4.8.1";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
 
