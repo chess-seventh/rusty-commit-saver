e235f704c809a8adc26f6fa61b8ecf619e0e7e67
Chess Seventh <25706993+chess-seventh@users.noreply.github.com>
2026-02-04 13:33:38 +0100

    feat: remove untested&unused code (#26)
    * refactor: Remove unused run_commit_saver from lib.rs
    
    - Remove the `run_commit_saver` function
    - Clean up related imports and comments in the library file
    - Eliminate associated logging and error handling components
    
    * chore: Update devenv for rusty-commit-hook
    
    - Update the 'system' reference in the development environment
      configuration
    - Fix from NixOS discourse thread: https://urlr.me/PvxTgb

--- devenv.nix
+++ devenv.nix
@@ -78,7 +78,7 @@
       stages = [ "post-commit" ];
       after = [ "commitizen" "gitlint" "gptcommit" ];
       entry = "${
-          inputs.rusty-commit-saver.packages.${pkgs.system}.default
+          inputs.rusty-commit-saver.packages.${pkgs.stdenv.hostPlatform.system}.default
         }/bin/rusty-commit-saver";
       pass_filenames = false;
       language = "system";
