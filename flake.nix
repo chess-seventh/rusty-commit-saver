f03561f8e0fceb50950f22cd1269965d8142d6db
chess-seventh <chess7th@pm.me>
2026-02-10 15:50:42 +0000

    chore(release): update flake.nix and Cargo.lock for 4.14.0 [skip ci]

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       in {
         packages.default = rustPlatform.buildRustPackage {
           pname = "rusty-commit-saver";
-          version = "4.13.0";
+          version = "4.14.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
 
